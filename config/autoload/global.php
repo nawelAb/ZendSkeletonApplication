3a42b4c2419a10097afea34384b0889a6203f9a9
nawelAb <naoual01ab@gmail.com>
2016-07-21 11:59:09 +0100

    ajout module auth

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -12,5 +12,23 @@
  */
 
 return array(
-    // ...
+     'db' => array(
+      'driver'         => 'Pdo',
+      'dsn'            => 'mysql:dbname=kwaret_1.0.0;host=localhost',
+      
+      'driver_options' => array(
+           'PDO::MYSQL_ATTR_INIT_COMMAND' => 'SET NAMES \'UTF8\''
+      ),
+      'username' => 'root', 
+      'password' => '', 
+    
+    ),
+   'service_manager' => array(
+      'factories' => array(
+         'Zend\Db\Adapter\Adapter' => 'Zend\Db\Adapter\AdapterServiceFactory',
+      ),
+   ),
+
+   'static_salt' => 'aFGQ475SDsdfsaf2342',
+
 );
