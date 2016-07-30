925570bf902193a0a01a40f4eb879d09f7b5896d
nawelAb <naoual01ab@gmail.com>
2016-07-30 19:40:23 +0100

    modification du module comments

---
+++ module/Comments/src/Comments/Controller/CommentsController.php
@@ -0,0 +1,17 @@
+<?php 
+namespace Comments\Controller;
+// filename : module/Forms/src/Forms/Controller/CommentsController.php
+
+use Zend\Mvc\Controller\AbstractActionController;
+// use Forms\Model\FormsModel;
+
+// use Forms\Form\FormsForm;
+// use Zend\Validator\File\Size; 
+
+class CommentsController extends AbstractActionController
+{
+    public function addCommentsAction() // UploadForm
+    {
+        var_dump('controller yesssssssss');die;
+    }
+}
