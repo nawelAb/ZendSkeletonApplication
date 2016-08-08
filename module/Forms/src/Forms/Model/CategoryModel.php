1b5133e49089fb250c89aceb36c1a5eb00efe233
Naoual Abbou <naoual.abbou@gmail.com>
2016-08-08 15:13:36 +0100

    modif de la vue dans application et ajut de category dans Forms

---
+++ module/Forms/src/Forms/Model/CategoryModel.php
@@ -0,0 +1,81 @@
+<?php
+// filename : module/Forms/src/Forms/Model/forms.php
+namespace Forms\Model;
+
+use Zend\InputFilter\Factory as InputFactory;
+use Zend\InputFilter\InputFilter;
+use Zend\InputFilter\InputFilterAwareInterface;
+use Zend\InputFilter\InputFilterInterface;
+
+use Zend\Filter\File\RenameUpload;
+use Zend\Validator\File\Size;
+
+class CategoryModel implements InputFilterAwareInterface
+{   
+    public $id;
+    public $value;
+    protected $inputFilter;
+    
+    public function exchangeArray($data)
+    {        
+        $this->id     = (isset($data['id'])) ? $data['id'] : null;
+        $this->value = (isset($data['value'])) ? $data['value'] : null;
+       
+    } 
+    
+    public function setInputFilter(InputFilterInterface $inputFilter)
+    {
+        throw new \Exception("Not used");
+    }
+    
+    public function getInputFilter()
+    {
+        if (!$this->inputFilter) {
+            $inputFilter = new InputFilter();
+            $factory     = new InputFactory();
+             
+            // $inputFilter->add(
+            //     $factory->createInput(array(
+            //         'name'     => 'form_name',
+            //         'required' => true,
+            //         'filters'  => array(
+            //             array('name' => 'StripTags'),
+            //             array('name' => 'StringTrim'),
+            //         ),
+            //         'validators' => array(
+            //             array(
+            //                 'name'    => 'StringLength',
+            //                 'options' => array(
+            //                     'encoding' => 'UTF-8',
+            //                     'min'      => 1,
+            //                     'max'      => 100,
+            //                 ),
+            //             ),
+            //         ),
+            //     ))
+            // );
+            
+            // $inputFilter->add(
+            //     $factory->createInput(array(
+            //         'name'     => 'fileUpload',
+            //         'required' => true,
+            //         'validators' => array(
+            //         //     [
+            //         //     'name'  => 'Zend\Validator\File\Size',
+            //         //     'options' =>
+            //         //         [
+            //         //             'max'      => '30MB',
+            //         //         ],
+            //         // ],
+            //         // 
+                        
+                        
+            //         )
+            //     ))
+            // );
+            
+            $this->inputFilter = $inputFilter;
+        }        
+        return $this->inputFilter;
+    }
+}
---
+++ module/Forms/src/Forms/Model/CategoryTable.php
@@ -0,0 +1,60 @@
+<?php
+namespace Forms\Model;
+
+use Zend\Db\TableGateway\TableGateway;
+use Zend\Db\ResultSet\ResultSet;
+
+
+class CategoryTable
+{
+    protected $tableGateway;
+
+    public function __construct(TableGateway $tableGateway)
+    {
+        $this->tableGateway = $tableGateway;
+    }
+	
+    public function fetchAll()
+    {
+        $resultSet = $this->tableGateway->select();
+        return $resultSet;
+    }
+
+    public function getForm($id)
+    {
+        $id  = (int) $id;
+        $rowset = $this->tableGateway->select(array('id' => $id));
+        $row = $rowset->current();
+        if (!$row) {
+            throw new \Exception("Could not find row $id");
+        }
+        return $row;
+    }
+
+    public function saveForm(FormsModel $form)
+    {
+        // pour Zend\Db\TableGateway\TableGateway les donnes doivent etre dans un tableau non un objet 
+        $data = array(           
+            'id'   => $form->id,
+            'form_name' => $form->form_name,                    
+        );
+        // a remplacer par  getArrayCopy() defini dans Auth
+        // $data = $form->getArrayCopy();
+
+        $id = (int)$form->id;
+        if ($id == 0) {
+            $this->tableGateway->insert($data);
+        } else {
+            if ($this->getUser($id)) {
+                $this->tableGateway->update($data, array('id' => $id));
+            } else {
+                throw new \Exception('Form id does not exist');
+            }
+        }
+    }
+    
+    public function deleteForm($id)
+    {
+        $this->tableGateway->delete(array('id' => $id));
+    }    
+}
