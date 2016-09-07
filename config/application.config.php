cfe0bc35633db51bfe95c33d74e853fbe790dd01
Naoual Abbou <naoual.abbou@gmail.com>
2016-09-07 10:42:30 +0100

    modif de kwaret/config

--- config/application.config.php
+++ config/application.config.php
@@ -9,6 +9,7 @@ return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
         'Application',
+        'TwbBundle', 
         'Acl',
         'Auth',
         'Forms', 
